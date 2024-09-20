3907c7fe0a08431aa2c9b8823d138e23cf06ca28
JeremyRoalef <roalef944@morrisville.edu>
2024-09-20 18:10:51 -0400

    Add Scenes || Add Scene Transitions

---
+++ ProjectBoost/Assets/Scripts/MainMenuUIHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onButtonPlayClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void onButtonShopClick()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+
+    public void onButtonTutorialClick()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void obButtonQuitClick()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
