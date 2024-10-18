9d955ef6ee9ee1378f7a3cb818f010f9df832295
JeremyRoalef <roalef944@morrisville.edu>
2024-10-18 18:41:28 -0400

    4.29 Trigger Player Explosion

--- ArgonAssault/Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ ArgonAssault/Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -5,14 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject explosionVFX;
+
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Collision event.\nMain Object: " + this.gameObject + "\nOther Object: " + other.gameObject);
     }
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
-        Debug.Log("Trigger event.\nMain Object: " + this.gameObject + "\nOther Object: " + other.gameObject);
+        explosionVFX.SetActive(true);
+        GetComponent<MeshRenderer>().enabled = false;
+        
+        StartCoroutine(StartDeathSequence());
+    }
+
+    IEnumerator StartDeathSequence()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
