06db98e662d7e290f86952fcd1a2bac640057dc4
JeremyRoalef <roalef944@morrisville.edu>
2024-10-25 16:46:26 -0400

    4.34 Enemy Hit Points

--- ArgonAssault/Argon Assault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Argon Assault/Assets/Scripts/Enemy.cs
@@ -6,22 +6,36 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explsionVFX;
     [SerializeField] Transform parent;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] int scoreValue;
+    [SerializeField] int intHp;
+    [SerializeField] int intDamageToTake;
 
     ScoreBoard scoreBoard;
 
     private void Awake()
     {
+        intHp = 100;
         scoreBoard = FindObjectOfType<ScoreBoard>(); //find the first ohject with the scoreboard script
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject vfx = Instantiate(explsionVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
-        scoreBoard.UpdateScore(scoreValue);
 
-        Destroy(gameObject);
+        intHp -= intDamageToTake;
+
+        if (intHp <= 0)
+        {
+            GameObject vfx = Instantiate(explsionVFX, transform.position, Quaternion.identity);
+            vfx.transform.parent = parent;
+            Destroy(gameObject);
+            scoreBoard.UpdateScore(scoreValue);
+        }
+        else
+        {
+            GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+            vfx.transform.parent = parent;
+        }
     }
 
 }
