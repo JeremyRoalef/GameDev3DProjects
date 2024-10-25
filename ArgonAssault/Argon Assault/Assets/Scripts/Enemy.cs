11f62a479a82cbaaf4c6bbc3e4f3ab1d7de254a7
JeremyRoalef <roalef944@morrisville.edu>
2024-10-25 17:08:14 -0400

    4.36 Using FindWithTag()
    FindWithTag() is no longer a method in the version of unity I am using. Instead, I used GameObject.FindGameObjectWIthTag()

--- ArgonAssault/Argon Assault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Argon Assault/Assets/Scripts/Enemy.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explsionVFX;
-    [SerializeField] Transform parent;
     [SerializeField] GameObject hitVFX;
     [SerializeField] int scoreValue;
     [SerializeField] int intHp;
     [SerializeField] int intDamageToTake;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     private void Awake()
     {
 
         Rigidbody rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
-
         intHp = 100;
         scoreBoard = FindObjectOfType<ScoreBoard>(); //find the first ohject with the scoreboard script
+    }
+
+    private void Start()
+    {
+        parentGameObject = GameObject.FindGameObjectWithTag("SpawnAtRuntime");
     }
 
     private void OnParticleCollision(GameObject other)
@@ -31,14 +35,14 @@ public class Enemy : MonoBehaviour
         if (intHp <= 0)
         {
             GameObject vfx = Instantiate(explsionVFX, transform.position, Quaternion.identity);
-            vfx.transform.parent = parent;
+            vfx.transform.parent = parentGameObject.transform;
             Destroy(gameObject);
             scoreBoard.UpdateScore(scoreValue);
         }
         else
         {
             GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-            vfx.transform.parent = parent;
+            vfx.transform.parent = parentGameObject.transform;
         }
     }
 
